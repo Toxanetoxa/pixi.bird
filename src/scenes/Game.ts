70a960824cc3afd78301f8dd8ae4493f442a85d9
Antonio Sotnik <toxanetoxa@gmail.com>
2023-04-10 08:53:19 +0300

    разбор кода Game

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -3,12 +3,18 @@ import ParallaxBackground from "../prefabs/ParallaxBackground";
 import { Player } from "../prefabs/Player";
 import Scene from "../core/Scene";
 
+/**
+ * Экспортируем класс игры и наследуем его от класса сцены
+ * **/
 export default class Game extends Scene {
 	name = "Game";
 
 	private player: Player | undefined;
 	private background: ParallaxBackground | undefined;
 
+	/**
+	 * оздается экземпляр класса ParallaxBackground для работы с параллакс-фоном и экземпляр класса Player для работы с игроком.
+	 * **/
 	load() {
 		this.background = new ParallaxBackground(config.backgrounds.forest);
 		this.player = new Player();
@@ -16,11 +22,15 @@ export default class Game extends Scene {
 		this.player.x = window.innerWidth / 2;
 		this.player.y = window.innerHeight - this.player.height / 3;
 
+		/**
+		 * Инициализируем движение фона за игроком**/
 		this.background.initPlayerMovement(this.player);
 
 		this.addChild(this.background, this.player);
 	}
 
+	/**
+	 * Метод для обработки изменения размеров экрана**/
 	onResize(width: number, height: number) {
 		if (this.player) {
 			this.player.x = width / 2;
